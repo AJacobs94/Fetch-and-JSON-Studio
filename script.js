31301502d9262c2519ee985c892d0ec16d403b01
AJacobs94 <56659280+AJacobs94@users.noreply.github.com>
2019-11-06 19:11:00 -0600

    added astronaut bios to page

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 // TODO: add code here
+window.addEventListener("load",function(){
+    let json=[];
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response){
+response.json().then(function(json){
+const container =document.getElementById('container');
+for (let i =0; i <json.length;i++){
+container.innerHTML=
+container.innerHTML+
+   `<div class ='astronaut'>
+   <div class = 'bio'>
+   <h3>${json[i].firstName}</h3>
+   <ul>
+   <li>${json[i].hoursInSpace}</li>
+   <li>${json[i].active}</li>
+   <li>${json[i].skills}</li>
+   </ul>
+   </div>
+   <img class= "avatar" src=${json[i].picture}
+   </div>
+
+   `
+}
+});
+    });
+});
